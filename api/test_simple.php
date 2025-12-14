702a8fa589b9d718e1310f7fde6440d180dc4a66
JoycelynAllan <joycelynallan177@gmail.com>
2025-12-14 00:39:05 +0000

    Explain what I changed

--- api/test_simple.php
+++
@@ -1,4 +0,0 @@
-<?php
-// Ultra-simple test to see if PHP even executes
-header('Content-Type: application/json');
-echo json_encode(['success' => true, 'message' => 'PHP works!', 'test' => 'basic']);
